079c83b8e9ebbc573d4f76bf66c4d8b5a6b11fe7
Andre Gurgel <andreggurgelufrn@gmail.com>
2021-05-25 18:01:13 -0300

    Projeto Iniciado

---
+++ src/main/java/br/dev/andregurgel/animelistapi/AnimelistApiApplication.java
@@ -0,0 +1,13 @@
+package br.dev.andregurgel.animelistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimelistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimelistApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/andregurgel/animelistapi/AnimelistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.andregurgel.animelistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimelistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
